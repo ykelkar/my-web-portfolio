c1c3b78b407d643585a64676b190ba878e249831
Yash <ykelk002@gmail.com>
2020-05-06 13:55:04 -0700

    Fix send grid issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const sendGrid = require('@sendGrid/mail');
+//const sendGrid = require('@sendGrid/mail');
 
 
 const app = express();
